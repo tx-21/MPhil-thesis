c3051b7314be0e89a5a1f655e961e32f9b42feca
Tim (win) <tim26901196@gmail.com>
2022-04-27 02:04:27 +0800

    Progress on 4/26.

--- Thesis/Chap-4-Resutls-and-Discussion/cov-testing.sh
+++ Thesis/Chap-4-Resutls-and-Discussion/cov-testing.sh
@@ -3,6 +3,7 @@ pandoc \
 --pdf-engine=xelatex \
 -V margin-left=20mm --number-sections \
 --filter pandoc-tablenos \
+--filter pandoc-fignos \
 -V fontsize=10pt \
 -V mainfont="Times New Roman" \
 -M tablenos-number-by-section=False \
